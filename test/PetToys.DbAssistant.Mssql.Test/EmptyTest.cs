e6fc46fd85cbf9d0212b0c9367596f3475892492
andbaz <dev@andbaz.slmail.me>
2025-04-17 14:52:44 +0300

    added empty solution structure

---
+++ test/PetToys.DbAssistant.Mssql.Test/EmptyTest.cs
@@ -0,0 +1,13 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace PetToys.DbAssistant.Mssql.Test;
+
+public sealed class EmptyTest
+{
+    [Fact]
+    public void Empty_Test()
+    {
+        true.Should().BeTrue();
+    }
+}
