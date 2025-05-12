8c728421c3b93c2c632876216d5f4a8fe8ed6c41
andbaz <dev@andbaz.slmail.me>
2025-05-12 16:20:40 +0300

    improving tests

--- test/PetToys.DbAssistant.Mssql.Test/Bulk/BulkInsertTest.cs
+++ test/PetToys.DbAssistant.Mssql.Test/Bulk/BulkInsertTest.cs
@@ -1,18 +1,19 @@
 ﻿using System.Diagnostics;
+using System.Threading;
 using System.Threading.Tasks;
 using Bogus;
 using FluentAssertions;
+using Microsoft.Data.SqlClient;
 using PetToys.DbAssistant.Mssql.Extensions;
 using PetToys.DbAssistant.Mssql.Test.Accessors;
 using Xunit;
-using Xunit.Abstractions;
 
-namespace PetToys.DbAssistant.Mssql.Test;
+namespace PetToys.DbAssistant.Mssql.Test.Bulk;
 
 [Trait("Category", "Integration")]
-public sealed class BulkInsertTest(ITestOutputHelper output) : DatabaseTestBase
+public sealed class BulkInsertTest(MsSqlFixture fixture, ITestOutputHelper output) : IClassFixture<MsSqlFixture>
 {
-    private const int BatchSize = 1_000;
+    private const int BatchSize = 1_000_000;
 
     private static readonly Faker<NullableEnabledEntity> FakeNullable = new Faker<NullableEnabledEntity>()
         .StrictMode(true)
@@ -37,11 +38,12 @@ public sealed class BulkInsertTest(ITestOutputHelper output) : DatabaseTestBase
         .RuleFor(e => e.Arr1, f => f.Random.Bytes(f.Random.Number(500)).OrNull(f, .1f));
 
     [Fact]
+    [Trait("DockerPlatform", "Linux")]
     public async Task NullableEnabled_Test()
     {
         var data = FakeNullable.Generate(BatchSize);
-        const string tableName = "nullable_test";
-        await using var connection = await ReCreateTableAsync(tableName);
+        const string tableName = "#nullable_test";
+        await using var connection = await OpenConnectionAndCreateTableAsync(tableName);
         var watch = Stopwatch.StartNew();
         var result = await connection.CreateBulkContext<NullableEnabledEntity>(tableName)
             .MapProperty(e => e.Int0)
@@ -52,18 +54,22 @@ public sealed class BulkInsertTest(ITestOutputHelper output) : DatabaseTestBase
             .MapProperty(e => e.Str1)
             .MapProperty(e => e.Arr0)
             .MapProperty(e => e.Arr1)
-            .WriteDataAsync(data);
+            .WriteDataAsync(data, cancellationToken: TestContext.Current.CancellationToken);
         watch.Stop();
         result.Should().Be(data.Count);
         output.WriteLine("Inserted {0:N0} rows. Elapsed time: {1:N0} ms.", result, watch.ElapsedMilliseconds);
+        var count = await ExecuteCountAsync(connection, tableName, TestContext.Current.CancellationToken);
+        count.Should().Be(data.Count);
+        await connection.CloseAsync();
     }
 
     [Fact]
+    [Trait("DockerPlatform", "Linux")]
     public async Task NotNullableEnabled_Test()
     {
         var data = FakeNotNullable.Generate(BatchSize);
-        const string tableName = "not_nullable_test";
-        await using var connection = await ReCreateTableAsync(tableName);
+        const string tableName = "#not_nullable_test";
+        await using var connection = await OpenConnectionAndCreateTableAsync(tableName);
         var watch = Stopwatch.StartNew();
         var result = await connection.CreateBulkContext<NullableDisabledEntity>(tableName)
             .MapProperty(e => e.Int0)
@@ -74,9 +80,45 @@ public sealed class BulkInsertTest(ITestOutputHelper output) : DatabaseTestBase
             .MapProperty(e => e.Str1, referenceNullable: true)
             .MapProperty(e => e.Arr0)
             .MapProperty(e => e.Arr1)
-            .WriteDataAsync(data);
+            .WriteDataAsync(data, cancellationToken: TestContext.Current.CancellationToken);
         watch.Stop();
         result.Should().Be(data.Count);
         output.WriteLine("Inserted {0:N0} rows. Elapsed time: {1:N0} ms.", result, watch.ElapsedMilliseconds);
+        var count = await ExecuteCountAsync(connection, tableName, TestContext.Current.CancellationToken);
+        count.Should().Be(data.Count);
+        await connection.CloseAsync();
+    }
+
+    private async Task<SqlConnection> OpenConnectionAndCreateTableAsync(string tableName)
+    {
+        var query = ($"""
+                      DROP TABLE IF EXISTS {tableName.QuoteName()};
+                      CREATE TABLE {tableName.QuoteName()} (
+                        [Int0] int NOT NULL
+                       ,[Int1] int
+                       ,[Date0] datetime NOT NULL
+                       ,[Date1] datetime
+                       ,[Str0] varchar(8000) NOT NULL
+                       ,[Str1] varchar(8000)
+                       ,[Arr0] varbinary(8000) NOT NULL
+                       ,[Arr1] varbinary(8000)
+                      );
+                      """);
+
+        var connection = (SqlConnection)fixture.CreateConnection();
+        await connection.OpenAsync();
+        await using var command = connection.CreateCommand();
+        command.CommandText = query;
+        await command.ExecuteNonQueryAsync();
+        return connection;
+    }
+
+    private static async ValueTask<int> ExecuteCountAsync(SqlConnection connection, string tableName, CancellationToken cancellationToken = default)
+    {
+        var query = $"SELECT COUNT(*) FROM {tableName};";
+        await using var command = connection.CreateCommand();
+        command.CommandText = query;
+        var result = await command.ExecuteScalarAsync(cancellationToken);
+        return (int?)result ?? 0;
     }
 }
---
+++ test/PetToys.DbAssistant.Mssql.Test/Bulk/MsSqlFixture.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Data.SqlClient;
+using Testcontainers.MsSql;
+using Testcontainers.Xunit;
+using Xunit.Sdk;
+
+namespace PetToys.DbAssistant.Mssql.Test.Bulk;
+
+public sealed class MsSqlFixture(IMessageSink messageSink)
+    : DbContainerFixture<MsSqlBuilder, MsSqlContainer>(messageSink)
+{
+    public override SqlClientFactory DbProviderFactory => SqlClientFactory.Instance;
+}
